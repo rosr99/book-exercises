b4fb750c25901ba9e27743259ef95d3c13638860
Ryan Ros <rosr99@yahoo.com>
2020-01-16 20:43:40 -0800

    completed versions of Chapter 5 exercise 1, Chapter 6 exercise 1, 2, and 3, Chapter 7 exercise 1, and 2

--- chapter-07-exercises/exercise-1/exercise.R
+++ chapter-07-exercises/exercise-1/exercise.R
@@ -3,38 +3,60 @@
 # Create a vector `names` that contains your name and the names of 2 people 
 # next to you. Print the vector.
 
+names <- c("Chris","Tim")
+print(names)
+
 
 # Use the colon operator : to create a vector `n` of numbers from 10:49
 
+n <- c(10:49)
+print(n)
 
 # Use the `length()` function to get the number of elements in `n`
 
+numberOfElements <- length(n)
+print(numberOfElements)
 
 # Add 1 to each element in `n` and print the result
 
+n <- n + 1
+print(n)
 
 # Create a vector `m` that contains the numbers 10 to 1 (in that order). 
 # Hint: use the `seq()` function
 
+m <- c(seq(10,1))
+print(m)
 
 # Subtract `m` FROM `n`. Note the recycling!
 
+n <- n - m
+print(n)
 
 # Use the `seq()` function to produce a range of numbers from -5 to 10 in `0.1`
 # increments. Store it in a variable `x_range`
 
+x_range <- seq(-5,10,by=0.1)
+print(x_range)
 
 # Create a vector `sin_wave` by calling the `sin()` function on each element 
 # in `x_range`.
 
+sin_wave <- sin(x_range)
+print(sin_wave)
 
 # Create a vector `cos_wave` by calling the `cos()` function on each element 
 # in `x_range`.
 
+cos_wave <- cos(x_range)
+print(cos_wave)
 
 # Create a vector `wave` by multiplying `sin_wave` and `cos_wave` together, then
 # adding `sin_wave` to the product
 
+wave <- c((sin_wave * cos_wave) + sin_wave)
+print(wave)
 
 # Use the `plot()` function to plot your `wave`!
 
+plot(wave)
